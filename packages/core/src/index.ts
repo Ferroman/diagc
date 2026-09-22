89acffda3e528603799a821a3ea7bb9cd1b4ec5c
Bogdan Frankovskyi <bfrankovskyi@gmail.com>
2026-09-22 20:05:07 +0300

    feat(core): m.plan() builder with zones, events, people and roles

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -46,6 +46,8 @@ export {
   CauseRef,
   ThreatModelBuilder,
   FlowRef,
+  PlanBuilder,
+  ZoneBuilder,
   type NodeOpts,
   type RelateOpts,
   type CommitOpts,
@@ -57,6 +59,8 @@ export {
   type ThreatOpts,
   type CommentOpts,
   type ElementOpts,
+  type ZoneOpts,
+  type EventOpts,
 } from './builder';
 export { validate, DiagramValidationError, IMAGE_REF, LIBRARY_IMAGE_REF, type ValidationIssue } from './validate';
 export { isDrawings, isLayoutOverlay } from './guards';
